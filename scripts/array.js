0c989b24a6d7ffe27fb3489555aacfd4fdeb11c5
M Babar <M Babar>
2016-12-21 23:15:36 +0500

    Added arrays experiment

---
+++ scripts/array.js
@@ -0,0 +1,8 @@
+// write a function that print all tags with a name and src attribute.
+// first find all tags with a name
+// check all tags that are returned by previous statement if it has src attribute.
+// If src attribute is present then print the src attribute on console.
+
+function printTags(tagName) {
+    var tags = document.getElementsByTagName()
+}
