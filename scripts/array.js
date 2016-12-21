87cd3ff95baf2bb6c3377ec5ed0c1ec66cd1ad36
M Babar <M Babar>
2016-12-21 23:16:15 +0500

    Updated array.js

--- scripts/array.js
+++ scripts/array.js
@@ -4,5 +4,11 @@
 // If src attribute is present then print the src attribute on console.
 
 function printTags(tagName) {
-    var tags = document.getElementsByTagName()
+    var tags = document.getElementsByTagName(tagName)
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+        if (tag.src) {
+            console.log(tag.src)
+        }
+    }
 }
